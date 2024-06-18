9ab50bf2347812893f263443965a4d383facf0c7
Anamay-Upreti <rcupretidgc13@gmail.com>
2024-06-18 22:54:02 +0530

    started-features

--- components/Features.tsx
+++ components/Features.tsx
@@ -1,8 +1,62 @@
 import React from 'react'
-
+import Image from 'next/image'
+import { FEATURES } from '@/constants'
+import { title } from 'process'
 const Features = () => {
   return (
-    <div>Features</div>
+    <section className='flex-col flexCenter overflow-hidden bg-feature-bg bg-center bg-no-repeat py-24'>
+          <div   className="max-container padding-container relative w-full justify-end">
+            <div className="flex flex-1 lg:min-h-[900px]
+            ">
+              <Image
+              src="/phone.png"
+              alt='phone'
+              width={440}
+              height={1000}
+              className='feature-phone'
+              />
+            </div>
+
+            <div className="z-20 flex w-full flex-col lg:w-[60%]">
+              <div className="relative ">
+                <Image
+                src="/camp.svg"
+                alt='camp'
+                width={50}
+                height={50}
+                className='absolute left-[-5px] top-[-28px] w-10 lg:w-[50px]'
+                />
+                <h2 className='bold-40 lg:bold-64'>Our Features</h2>
+              </div>
+              <ul>
+                {FEATURES.map((feature) => (
+                  <FeatureItem
+                  key={feature.title}
+                  title={feature.title}
+                  icon={feature.icon}
+                  variant={feature.variant}
+                  description={feature.description}
+                  />
+                ))}
+              </ul>
+            </div>
+          </div>
+    </section>
+  )
+}
+
+type FeatureItem = {
+  title: string
+  icon: string
+  variant: string
+  description: string
+}
+
+const FeatureItem = ({ title, icon, variant, description }:FeatureItem) => {
+  return(
+    <li className=''>
+      {title}
+    </li>
   )
 }
 
